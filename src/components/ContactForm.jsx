3a4da61ccd8e842525a53e657d763b50385947f6
pavelnbc <giperpasha@rambler.ru>
2018-05-11 12:42:23 +0300

    Updates for 11.05

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -27,29 +27,6 @@ class ContactForm extends Component {
                             Sun: day off <br/>
                         </p>
                     </div>
-                    {/*<table>
-                        <tbody>
-                            <tr>
-                                <td>10 Oak street, <br/> Austin, TX, 35789</td>
-                            </tr>
-                            <tr>
-                                <td>random@aol.com</td>
-                            </tr>
-                            <tr>
-                                <td>Tel. number: <br/> +1(452) 683-33-95</td>
-                            </tr>
-                            <tr>
-                                <td>Working hours:</td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    Mon-Fri: 9:00-19:00 <br/>
-                                    Sat: 10:00-14:00 <br/>
-                                    Sun: day off <br/>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>*/}
                     <form className="contact-form__form">
                         <input type="text" placeholder="Name"/>
                         <input type="text" placeholder="Email"/>
